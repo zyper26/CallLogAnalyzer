d41fd3354c34aa1ba745b3ea8189240c9d867095
zyper26 <shubham.chaurasia.261996@gmail.com>
2020-05-24 16:49:16 +0530

    added button in main

--- app/src/main/java/com/example/socialization/MainActivity.java
+++ app/src/main/java/com/example/socialization/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.socialization;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +10,6 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity  {
 
     public static String TAG = MainActivity.class.getSimpleName();
-//    CallReceiver callReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +20,10 @@ public class MainActivity extends AppCompatActivity  {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
+                Intent intent = new Intent(MainActivity.this, SocializingOnlineActivity.class);
+                startActivityForResult(intent, 1);
             }
         });
     }
+
 }
